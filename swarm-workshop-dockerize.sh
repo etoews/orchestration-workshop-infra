57513079a13b976970a7761edb0ee944b905f1d1
Everett Toews <everett.toews@rackspace.com>
2016-09-12 10:39:24 -0500

    Scripts for Swarm infra on Rackspace

---
+++ swarm-workshop-dockerize.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ENGINE_VERSION=1.12.1-0~xenial
+COMPOSE_VERSION=1.8.0
+
+useradd -d /home/docker -m -s /bin/bash docker
+echo docker:training | chpasswd
+
+tee /etc/sudoers.d/docker <<SQRL
+docker ALL=(ALL) NOPASSWD:ALL
+SQRL
+
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+service ssh restart
+
+apt-get -q update
+apt-get remove -y --purge dnsmasq-base
+apt-get -qy install fail2ban git httping apache2-utils htop
+
+# broken
+# echo 1000000 > /proc/sys/net/nf_conntrack_max
+
+apt-get -y install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+
+apt-get -q update
+apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get -y install docker-engine=${ENGINE_VERSION}
+
+sed -i 's,-H fd://$,-H fd:// -H tcp://0.0.0.0:55555,' /lib/systemd/system/docker.service
+systemctl daemon-reload
+curl --silent localhost:55555 || sudo systemctl restart docker
+systemctl start docker || true
+
+wget -q -O /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m`
+chmod +x /usr/local/bin/docker-compose
+
+sudo -u docker mkdir -p /home/docker/.ssh
+sudo -u docker echo StrictHostKeyChecking no > /home/docker/.ssh/config
+cat /root/.ssh/authorized_keys > /home/docker/.ssh/authorized_keys
+chown docker:docker /home/docker/.ssh/authorized_keys
+
+docker pull redis
+docker pull ruby:alpine
+docker pull python:alpine
+docker pull node:4-slim
+docker pull registry:2
+docker pull golang
+docker pull busybox
+
+# debug cloud-init
+# less /var/log/cloud-init-output.log
---
+++ swarm-workshop.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Usage:
+#   swarm-workshop.sh create
+#   swarm-workshop.sh version
+#   swarm-workshop.sh hello
+#   swarm-workshop.sh clusterize
+#   dockerswarm100.sh delete
+# Requirements:
+#   Environment Variables:
+#     export RS_USERNAME={my-rackspace-username}
+#     export RS_API_KEY={my-rackspace-api-key}
+#   rack: The Rackspace command line interface for managing Rackspace services
+#     https://developer.rackspace.com/docs/rack-cli/configuration/#installation-and-configuration
+#   Keypair named swarm
+#     ssh-keygen -q -b 4096 -t rsa -N "" -f ~/.ssh/id_rsa.swarm
+#     for region in IAD; do
+#       export RS_REGION_NAME=${region}
+#       rack servers keypair upload --name swarm --file ~/.ssh/id_rsa.swarm.pub
+#     done
+
+main() {
+  echo "$@"
+  loop $@
+}
+
+loop() {
+  for region in IAD; do
+    export RS_REGION_NAME=${region}
+    echo -e "Region\t\t${RS_REGION_NAME}"
+
+    for u in $(seq -f "%02g" 1 1); do
+      for n in $(seq 1 5); do
+        USERNAME=user${u}
+        SERVER_NAME=${USERNAME}-node${n}
+        echo -e "Name\t\t${SERVER_NAME}"
+        "$@"
+      done
+    done
+  done
+}
+
+create() {
+  rack servers instance create \
+    --region ${RS_REGION_NAME}
+    --name ${SERVER_NAME} \
+    --image-name "Ubuntu 16.04 LTS (Xenial Xerus) (PVHVM)" \
+    --flavor-name "1 GB General Purpose v1" \
+    --keypair swarm \
+    --user-data swarm-workshop-dockerize.sh
+}
+
+clusterize() {
+  IFS=$'\n'
+  hosts=""
+  nodes=$(rack servers instance list --name ${USERNAME} --fields name,publicipv4,privateipv4 --no-header)
+  for node in ${nodes[@]}; do
+    hostname=$(echo ${node} | awk '{print $1}' | awk -F'-' '{print $2}')
+
+    # publicipv4=$(echo ${node} | awk '{print $2}')
+    privateipv4=$(echo ${node} | awk '{print $3}')
+
+    # hosts="${hosts}\\\n${publicipv4}\\\t${hostname}"
+    hosts="${hosts}\\\n${privateipv4}\\\t${hostname}"
+  done
+
+  SERVER_IP=$(get_server_ip)
+  scp -i ~/.ssh/id_rsa.swarm ~/.ssh/id_rsa.swarm docker@${SERVER_IP}:.ssh/id_rsa
+  ssh -i ~/.ssh/id_rsa.swarm root@${SERVER_IP} "/bin/bash -c 'echo -e ${hosts} >> /etc/hosts'"
+}
+
+delete() {
+  rack servers instance delete --name ${SERVER_NAME}
+}
+
+version() {
+  SERVER_IP=$(get_server_ip)
+  DOCKER_VERSION=$(ssh -i ~/.ssh/id_rsa.swarm root@${SERVER_IP} docker version | grep Version | tail -n 1 | awk '{print $2}')
+  echo -e "Version\t\t${DOCKER_VERSION}"
+}
+
+hello() {
+  SERVER_IP=$(get_server_ip)
+  ssh -i ~/.ssh/id_rsa.swarm root@${SERVER_IP} docker run alpine echo "Hello World"
+}
+
+info() {
+  SERVER_IP=$(get_server_ip)
+  ssh -i ~/.ssh/id_rsa.swarm root@${SERVER_IP} docker info | awk '/Swarm/,/Runtimes/' | sed '$ d'
+}
+
+get_server_ip() {
+  echo $(rack servers instance get --name ${SERVER_NAME} --fields publicipv4 | awk '{print $2}')
+}
+
+main $@
